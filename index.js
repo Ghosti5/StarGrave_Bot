e5ce0957d62bfeab5664ce4cfa143a01789d0037
Ghosti5 <iammaddieatyou@gmail.com>
2024-02-24 01:39:05 -0700

    AAA

--- index.js
+++ index.js
@@ -357,7 +357,7 @@ client.on('interactionCreate', async interaction => {
 
 app.post('/interactions', async (req, res) => {
     try {
-        const interaction = req.body;
+        const interaction = req.rawHeaders;
         console.log(interaction);
         
 
