588d85c24fac194c1b46a49b7aa633cf6c2dbb51
Ghosti5 <iammaddieatyou@gmail.com>
2024-02-23 19:18:02 -0700

    FSAF

--- index.js
+++ index.js
@@ -355,7 +355,7 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-app.put('/interactions', async (req, res) => {
+app.post('/interactions', async (req, res) => {
     try {
         const interaction = req.data;
         console.log(interaction);
